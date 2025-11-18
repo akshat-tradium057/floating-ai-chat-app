81449452974e961df703d7e5590b7d37e740e06f
akshat-tradium057 <akshat.singh@tradiuminc.com>
2025-11-18 16:27:57 +0530

    changed name and null checks

--- src/components/BotAndUserChat.tsx
+++ src/components/BotAndUserChat.tsx
@@ -9,18 +9,18 @@ export default ({ messagesData }) => {
           return (
             <>
               <Switch>
-                <Match when={item.role === "CHATBOT"}>
+                <Match when={item?.role === "CHATBOT"}>
                   <p
                     class="ml-auto w-32 bg-slate-100 p-2 rounded-xl text-sm "
-                    id={item.id}
+                    id={item?.id}
                   >
                     {item?.text || ""}
                   </p>
                 </Match>
-                <Match when={item.role === "USER"}>
+                <Match when={item?.role === "USER"}>
                   <p
                     class={`mr-auto max-w-44 p-2 rounded-xl text-sm text-white ${GRADIENT_CLASS} `}
-                    id={item.id}
+                    id={item?.id}
                   >
                     {item?.text || ""}
                   </p>
